738e8fc070d32b86e2a06f7628136ea551be02b4
Winston T <54168818+wtsia@users.noreply.github.com>
2022-02-28 13:17:00 -0800

    updated css, still figuring out why search form data isnt working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const parser = require('body-parser');
 const methodOverride = require('method-override');
 const dotEnv = require('dotenv').config();
-const parser = require('body-parser');
 const hbs = require('hbs');
 const path = require('path')
 const controller = require('./controllers/controller');
@@ -10,12 +10,13 @@ const app = express();
 
 
 app.set("view engine", "hbs");
-app.use(cors())
+app.use(cors());
+app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(parser.json());
 app.use("/", controller);
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('views/images')); 
 app.set("port", process.env.PORT || 3301);
 
 app.listen(app.get("port"), () => {
